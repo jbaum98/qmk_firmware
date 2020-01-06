2e26c7628bd5107598b64a757f55b216c4d93875
Jake Waksbaum <jake.waksbaum@gmail.com>
2020-01-06 15:06:53 -0500

    Switch * and -

--- keyboards/planck/keymaps/jbaum98/keymap.c
+++ keyboards/planck/keymaps/jbaum98/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_ASTR, KC_UNDS, _______,  _______,  _______,
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_MINS, KC_UNDS, _______,  _______,  _______,
     _______, KC_GRV,  KC_ASTR, KC_BSLS, KC_PIPE, KC_PLUS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_DQUO, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______, KC_TILD, KC_LBRC, KC_LCBR, KC_LPRN, KC_EQUAL, KC_MINS, KC_RPRN, KC_RCBR, KC_RBRC, KC_QUOT, _______,
+    _______, KC_TILD, KC_LBRC, KC_LCBR, KC_LPRN, KC_EQUAL, KC_ASTR, KC_RPRN, KC_RCBR, KC_RBRC, KC_QUOT, _______,
     _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______
 ),
